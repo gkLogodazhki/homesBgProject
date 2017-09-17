0a6bedbb6b87b0727279bb2c2db7769585475f15
gkLogodazhki <31129502+gkLogodazhki@users.noreply.github.com>
2017-09-17 18:03:30 +0300

    Add RealEstateCompany with lil Demo

---
+++ src/homesbg/demoAdAndEstate/Demo.java
@@ -0,0 +1,95 @@
+package demoAdAndEstate;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.google.gson.Gson;
+
+import ad.Ad;
+import ad.AdLoan;
+import ad.Place;
+import ad.Place.City;
+import ad.Place.INeighborhood;
+import estate.Apartment;
+import estate.Building.ConstructionKind;
+import estate.Building.Creation;
+import estate.Building.Furnishing;
+import estate.Building.Heating;
+import estate.Estate.EstateKind;
+import estate.Estate.Extrable;
+import ad.Ad.Currency;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		RealEstateCompany rec = new RealEstateCompany("GMRealEstates");
+		TreeSet<Extrable> extrables = new TreeSet<>((o1,o2)-> o1.toString().compareTo(o2.toString()));
+		do {
+			extrables.add(Extrable.Apartment.values()[new Random().nextInt(Extrable.Apartment.values().length)]);
+		} while (extrables.size() != Extrable.Apartment.values().length/2);
+		for (Extrable e : Extrable.BuildingLoan.values()) {
+			extrables.add(e);
+		}
+		Set<File> pictures = new HashSet<>();
+
+		Ad ad1 = new AdLoan(10001, Currency.LEVA
+				, new Apartment(110, EstateKind.BUILDING, extrables, "Some things"
+				, new Place(City.SOFIA, INeighborhood.Sofia.LOZENEC, "James Boucher 3")
+				, true, 7, Furnishing.FURNISHED, Creation.AFTER_2006, ConstructionKind.BRICK, Heating.LOCAL, 4)
+				
+//				, new Agency() // error with Agency.class implementation, comment by Georgi L.
+				, null
+				
+				, pictures
+				, false, "3", true);
+		
+		Ad ad2 = new AdLoan(12000, Currency.$
+				, new Apartment(150, EstateKind.BUILDING, extrables, "Some things"
+				, new Place(City.SOFIA, INeighborhood.Sofia.MLADOST, "James Boucher 3")
+				, true, 7, Furnishing.UNFURNISHED, Creation._2000_2006, ConstructionKind.EPK, Heating.ELECTRICAL, 4)
+				
+//				, new Agency() // error with Agency.class implementation, comment by Georgi L.
+				, null
+				
+				, pictures
+				, false, "5", true);
+		
+		rec.addAd(ad1);
+		rec.addAd(ad2);
+		
+		File f = new File("demoHomesBg.txt");
+		
+		try {
+			f.createNewFile();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		inputJsonToFile(new Gson().toJson(ad1),f);
+		inputJsonToFile(new Gson().toJson(ad2),f);
+		
+	}
+	
+	public static void inputJsonToFile(String json, File f) {
+				
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(f,true))) {
+			bw.newLine();
+			bw.write(json);
+			bw.newLine();
+			bw.flush();
+			System.out.println("input json to file complite!");
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/homesbg/demoAdAndEstate/RealEstateCompany.java
@@ -0,0 +1,51 @@
+package demoAdAndEstate;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ad.Ad;
+import ad.Ad.AdType;
+import registration.contactData.ContactData;
+import registration.contactData.ContactData.ContactDataType;
+
+public class RealEstateCompany {
+
+	private String name;
+	private Map<ContactDataType,Set<ContactData>> users;
+	private Map<AdType,Set<Ad>> ads;
+	
+	public RealEstateCompany(String name) {
+		this.name = name;
+		this.users = new HashMap<>();
+		this.ads = new HashMap<>();
+	}
+		
+	public void addUser(ContactData cd) {
+		if (!this.users.containsKey(cd.getContactDataType())) {
+			this.users.put(cd.getContactDataType(), new HashSet<>());
+		}
+		if (this.users.get(cd.getContactDataType()).contains(cd)) {
+			System.out.println("The ad already exist in our database");
+		} else {
+			this.users.get(cd.getContactDataType()).add(cd);
+		}
+	}
+	
+	public void addAd(Ad ad) {
+		if (!this.ads.containsKey(ad.getAdType())) {
+			this.ads.put(ad.getAdType(), new HashSet<>());
+		}
+		if (this.ads.get(ad.getAdType()).contains(ad)) {
+			System.out.println("The ad already exist in our database");
+		} else {
+			this.ads.get(ad.getAdType()).add(ad);
+		}
+	}
+
+	public String getName() {
+		return name;
+	}	
+	
+}
